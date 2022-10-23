4a5e23b5d2592fc730ccffd899ef620c2a812ece
Or Geisler <orgeisler1@gmail.com>
2022-10-23 12:59:04 +0300

    server tests

--- backend/tests/backend_tests/server_test.py
+++ backend/tests/backend_tests/server_test.py
@@ -1,5 +1,21 @@
-# import mock
+import requests
 
-# @mock.patch()
-def test_sum():
-    assert 1+1 == 2
+def test_get_recipe():
+    response = requests.get(f'http://127.0.0.1:8001/recipes/cheese')
+    assert isinstance(response,requests.models.Response)==True
+    assert response.status_code==200
+
+def test_get_recipe_gluten_free():
+    response = requests.get(f'http://127.0.0.1:8001/recipes/cheese?gluten_free=True')
+    assert isinstance(response,requests.models.Response)==True
+    assert response.status_code==200
+
+def test_get_recipe_dairy_free():
+    response = requests.get(f'http://127.0.0.1:8001/recipes/cheese?dairy_free=True')
+    assert isinstance(response,requests.models.Response)==True
+    assert response.status_code==200
+
+def test_get_recipe_dairy_gluten_free():
+    response = requests.get(f'http://127.0.0.1:8001/recipes/cheese?dairy_free=True&gluten_free=True')
+    assert isinstance(response,requests.models.Response)==True
+    assert response.status_code==200
