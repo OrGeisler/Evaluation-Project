422260e73ed611065d270cdf9ff72ce6391200f1
Or Geisler <orgeisler1@gmail.com>
2022-10-23 08:57:15 +0300

    status code- first router

--- backend/routers/first_router/first_router.py
+++ backend/routers/first_router/first_router.py
@@ -1,14 +1,15 @@
-from fastapi import APIRouter,HTTPException,status
+from fastapi import APIRouter,HTTPException,status,Response
 import requests
 from . import first_router_utils
 from ..db_utils import querys
 from ..db_utils.db_proxy import db_proxy
+from fastapi.responses import JSONResponse
 
 firstRoute = APIRouter()
 
 
-@firstRoute.get('/players',status_code=status.HTTP_200_OK)
-def root(teamName,year):
+@firstRoute.get('/players',status_code=status.HTTP_200_OK , response_class= JSONResponse)
+def root(teamName,year,response:Response):
 
     ## put the http call in the utils to catc the error
 
@@ -17,4 +18,5 @@ def root(teamName,year):
         players_list = first_router_utils.fillterByteam(players_data,teamName)
         return players_list 
     except:
+        response.status_code = status.HTTP_404_NOT_FOUND
         raise HTTPException(status_code=404, detail="Team stats does not exist")
